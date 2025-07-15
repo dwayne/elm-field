426eaf0bfed2db7b285cece924d07703a3516976
Dwayne Crooks <me@dwaynecrooks.com>
2025-07-15 07:14:47 -0400

    Start adding tests for Field.int

--- tests/Test/Field.elm
+++ tests/Test/Field.elm
@@ -2,13 +2,30 @@ module Test.Field exposing (suite)
 
 import Expect
 import Field as F
-import Test exposing (Test, describe, test)
+import Fuzz
+import Test exposing (Test, describe, fuzz, test)
 
 
 suite : Test
 suite =
     describe "Field"
-        [ test "1 + 1" <|
-            \_ ->
-                (1 + 1) |> Expect.equal 2
+        [ intSuite
+        ]
+
+
+intSuite : Test
+intSuite =
+    describe "Int"
+        [ describe "int"
+            [ fuzz Fuzz.int "all integer strings in the range Random.minInt to Random.maxInt inclusive" <|
+                \n ->
+                    F.fromString F.int (String.fromInt n)
+                        |> F.toMaybe
+                        |> Expect.equal (Just n)
+            , fuzz Fuzz.int "all integers in the range Random.minInt to Random.maxInt inclusive" <|
+                \n ->
+                    F.fromValue F.int n
+                        |> F.toMaybe
+                        |> Expect.equal (Just n)
+            ]
         ]
